c08321665b4cf68209024f8e349de1100547f7ae
atanukundu10 <kunduatanu36@gmail.com>
2024-04-09 21:18:48 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,44 @@
+const url = "https://api.dictionaryapi.dev/api/v2/entries/en/";
+
+const result = document.getElementById("result")
+const sound = document.getElementById("sound")
+const btn = document.getElementById("search-btn")
+
+function playSound(){
+    sound.play()
+}
+
+btn.addEventListener("click", () =>{
+    let inpWord = document.getElementById("input").value;
+
+    fetch(`${url}${inpWord}`)
+    .then((response) => response.json())
+    .then((data) => {
+        result.innerHTML = `<div class="word">
+        <h3>${inpWord}</h3>
+        <button onclick="playSound()">
+            <i class="fa-solid fa-volume-high"></i>
+        </button>
+        </div>
+
+        <div class="details">
+            <p>${data[0].meanings[0].partOfSpeech}</p>
+            <p>/${data[0].phonetics[1].text}/</p>
+        </div>
+
+        <p class="word-meaning">
+            ${data[0].meanings[0].definitions[0].definition} 
+        </p>
+        <p class="word-example">
+            ${data[0].meanings[0].definitions[0].example || ""} 
+        </p>`
+
+        sound.setAttribute("src", `${data[0].phonetics[0].audio}`)
+    })
+    .catch( () => {
+        result.innerHTML = `<p class="error"> Unable to find the word </p>`
+    })
+})
+
+
+
